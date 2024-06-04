219616fc362e1455a1b9621e7aebee1920680695
IronLlama <ovioprime@gmail.com>
2024-06-04 19:49:08 +0100

    Initial. Works.

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const KUROI = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+KUROI.abilities = {
+  str: 'KUROI.Ability.Str.long',
+  dex: 'KUROI.Ability.Dex.long',
+  con: 'KUROI.Ability.Con.long',
+  int: 'KUROI.Ability.Int.long',
+  wis: 'KUROI.Ability.Wis.long',
+  cha: 'KUROI.Ability.Cha.long',
+};
+
+KUROI.abilityAbbreviations = {
+  str: 'KUROI.Ability.Str.abbr',
+  dex: 'KUROI.Ability.Dex.abbr',
+  con: 'KUROI.Ability.Con.abbr',
+  int: 'KUROI.Ability.Int.abbr',
+  wis: 'KUROI.Ability.Wis.abbr',
+  cha: 'KUROI.Ability.Cha.abbr',
+};
